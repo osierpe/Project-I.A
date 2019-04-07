ab37ca851268a45c87e1051a6022c4d2493af7c8
osierpe <45239912+osierpe@users.noreply.github.com>
2019-04-07 16:37:25 +0100

    Add files via upload

---
+++ EvaluationFunction3.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EvaluationFunction
+{
+    // Do the logic to evaluate the state of the game !
+    public float evaluate(State s, bool max)
+    {
+        float val = 0;
+        Unit current = s.unitToPermormAction;
+        Tuple<float, float> bonus = current.GetBonus(s.board, s.PlayersUnits); //(currentHpBonus, currentAttackBonus)
+
+        //Sum of our HP 
+        for (int i = 0; i < s.PlayersUnits.Count; i++)
+        {
+            val  += s.PlayersUnits[i].hp;
+        }
+        //Subtraction of enemy's HP
+        for (int k = 0; k < s.AdversaryUnits.Count; k++)
+        {
+            val -= s.AdversaryUnits[k].hp;
+        }
+
+        if (s.isAttack) // If Attack State
+        {
+            if (!max) //If is not a max State
+            {
+                Unit attacked = s.unitAttacked;
+                val += 200;
+
+                //Favour attacks where our HP is greater or equal to the unit attacked's HP
+                if (current.hp >= attacked.hp)
+                {
+                    val += 200;
+                }
+                else
+                {
+                    val -= 100;
+                }
+                //Favour attacks where we have bonus(es)
+                if (bonus.Item2 > 0 && bonus.Item1 > 0)
+                {
+                    val += 200;
+                }
+                else if (bonus.Item2 > 0)
+                {
+                    val += 75;
+                }
+                else if (bonus.Item1 > 0)
+                {
+                    val += 50;
+                }
+
+            }
+            if (max) //If is a max State
+            {
+                Unit attacked = s.unitAttacked;
+                val -= 200;
+
+                //Favour attacks where them HP is less to the unit attacked's HP
+                if (current.hp >= attacked.hp)
+                {
+                    val -= 200;
+                }
+                else
+                {
+                    val += 100;
+                }
+                //Favour attacks where they have not bonus(es)
+                if (bonus.Item2 > 0 && bonus.Item1 > 0)
+                {
+                    val -= 200;
+                }
+                else if (bonus.Item2 > 0)
+                {
+                    val -= 100;
+                }
+                else if (bonus.Item1 > 0)
+                {
+                    val -= 100;
+                }
+
+            }
+        }
+        else //If Move State
+        {
+            //Favour moves where we keep/have bonuses 
+            if (!max)
+            {
+                if (bonus.Item2 > 0 && bonus.Item1 > 0)
+                {
+                    val += 200;
+                }
+                else if (bonus.Item2 > 0)
+                {
+                    val += 75;
+                }
+                else if (bonus.Item1 > 0)
+                {
+                    val += 50;
+                }
+            }
+            else //the opposite for their move
+            {
+                if (bonus.Item2 > 0 && bonus.Item1 > 0)
+                {
+                    val -= 200;
+                }
+                else if (bonus.Item2 > 0)
+                {
+                    val -= 75;
+                }
+                else if (bonus.Item1 > 0)
+                {
+                    val -= 50;
+                }
+            }
+        }
+        
+        return val;
+    }
+}
---
+++ UtilityFunction2.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UtilityFunction
+{
+    private float value;
+
+    /*
+     * The bool parameter indicates wether ValMax or ValMin called the function
+     *  In case of Valmin, the PlayerUnits and the AdversaryUnits are switched in the respective state being evaluated  
+     *  
+     *  If the algorithm loses, it subtracts 200 and the Sum of the enemie's HP from the global variable "value";
+     *  Else if the algotihm wins, it adds 200 + the sum of all it's players' HP to the global variable "value"
+     *  In the end, it simply returns "value"
+    */
+
+    public float evaluate(State s, bool Max)
+    {
+        value = 0;
+        if (Max)
+        {
+            if (s.PlayersUnits.Count == 0) // We (Max) LOST
+            {
+                foreach (Unit adv in s.AdversaryUnits)
+                {
+                    value -= adv.hp;
+                }
+                value -= 100000;
+            }
+
+            else
+            { // We (Max) WON
+                foreach (Unit u in s.PlayersUnits)
+                {
+
+                    value += u.hp;
+                }
+                value += 100000;
+                value -= 100 * s.depth; // the less the depth, the greater the value
+            }
+        }
+        else
+        {
+            if (s.PlayersUnits.Count == 0) // We (Max) WON
+            {
+                value += 100000;
+                value -= 100*s.depth; // the less the depth, the greater the value
+                foreach (Unit u in s.AdversaryUnits)
+                {
+                    value += u.hp;
+                }
+            }
+
+            else // We (Max) LOST
+            {
+                value -= 100000;
+                foreach (Unit u in s.PlayersUnits)
+                {
+                    value -= u.hp;
+                }
+            }
+        }
+        return value;
+    }
+}
